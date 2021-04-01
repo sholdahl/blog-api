114a1b58172e3de65c780835a6ff18f12255fe35
sholdahl <sam.holdahl@gmail.com>
2021-03-31 21:52:40 -0500

    set up basic post route

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ let logger = require('morgan');
 
 let indexRouter = require('./routes/index');
 let usersRouter = require('./routes/users');
+let postsRouter = require('./routes/posts');
+
 
 let app = express();
 
@@ -28,6 +30,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/posts', postsRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
