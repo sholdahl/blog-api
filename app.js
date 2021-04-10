1123f22a437371989ebfbc9111fe8a087b59da7d
sholdahl <sam.holdahl@gmail.com>
2021-04-10 14:34:43 -0500

    user controller completed so all routes work

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ let express = require('express');
 let path = require('path');
 let cookieParser = require('cookie-parser');
 let logger = require('morgan');
+let bodyParser = require('body-parser')
 
 let usersRouter = require('./routes/users');
 let postsRouter = require('./routes/posts');
@@ -22,6 +23,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/users', usersRouter);
